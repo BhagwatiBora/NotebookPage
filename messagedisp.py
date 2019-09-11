534f9ac950c3bdb0bab2443e547740fc6a75203b
BhagwatiBora <54991538+BhagwatiBora@users.noreply.github.com>
2019-09-11 21:39:17 +0530

    Add files via upload

---
+++ messagedisp.py
@@ -0,0 +1,15 @@
+#wap tht prompts the user to enter first & last name then dispaly the following messages
+(#hello
+#first name
+#last name
+#welcome to python)
+
+
+print("enter the first name=")
+f=input()
+print("enter the last name=")
+l=input()
+com=f+l
+print("\nHELLO!")
+print("complete name="+com)
+print("WELCOME TO PYTHON")
